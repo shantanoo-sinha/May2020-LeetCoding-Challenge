2cc1fcee9bdebca601b6b381c2254ae9c14ab58c
Shantanoo Sinha <shantanoo.sinha@gmail.com>
2020-05-24 15:56:26 -0500

    Create construct-binary-search-tree-from-preorder-traversal.java

---
+++ Week4:May22nd-May28th/construct-binary-search-tree-from-preorder-traversal.java
@@ -0,0 +1,54 @@
+/*
+
+Reference: https://leetcode.com/problems/construct-binary-search-tree-from-preorder-traversal/
+
+1008. Construct Binary Search Tree from Preorder Traversal
+
+Return the root node of a binary search tree that matches the given preorder traversal.
+
+(Recall that a binary search tree is a binary tree where for every node, any descendant of node.left has a value < node.val, and any descendant of node.right has a value > node.val.  Also recall that a preorder traversal displays the value of the node first, then traverses node.left, then traverses node.right.)
+
+It's guaranteed that for the given test cases there is always possible to find a binary search tree with the given requirements.
+
+Example 1:
+
+Input: [8,5,1,7,10,12]
+Output: [8,5,10,1,7,null,12]
+
+
+Constraints:
+
+    1 <= preorder.length <= 100
+    1 <= preorder[i] <= 10^8
+    The values of preorder are distinct.
+
+*/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int i = 0;
+    public TreeNode bstFromPreorder(int[] preorder) {
+        return bstFromPreorder(preorder, Integer.MAX_VALUE);
+    }
+
+    public TreeNode bstFromPreorder(int[] preorder, int bound) {
+        if (i == preorder.length || preorder[i] > bound) return null;
+        TreeNode root = new TreeNode(preorder[i++]);
+        root.left = bstFromPreorder(preorder, root.val);
+        root.right = bstFromPreorder(preorder, bound);
+        return root;
+    }
+}
